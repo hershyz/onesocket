0b37c4b2fa0eecc5c448bc386c4174be60fed1a4
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2023-04-01 23:25:10 -0400

    Update onesocket.py
    make server invincible

--- onesocket.py
+++ onesocket.py
@@ -16,32 +16,37 @@ class OneSocketAPI:
         # main loop
         while True:
             
-            # parse raw request, get json data
-            client_connection, client_address = listen_socket.accept()
-            request_data_arr = client_connection.recv(1024).decode('utf-8').split('\n')
-            request_data = ''
-            json_data = {}
-            for element in request_data_arr:
-                if len(element) > 0 and element[0] == '{':
-                    request_data = element
-                    break
-            if request_data != '':
-                json_data = json.loads(request_data)
-
-            # handle response
-            endpoint = json_data['endpoint']
-            endpoint_object = endpoint_objects[endpoint]
-            res = ''
             try:
-                res = endpoint_object.handle(json_data)
-            except Exception as e:
-                res = str(e)
-            
-            # format and send response
-            http_response = b"""\
-        HTTP/1.1 200 OK
-        Access-Control-Allow-Origin: *
+                # parse raw request, get json data
+                client_connection, client_address = listen_socket.accept()
+                request_data_arr = client_connection.recv(1024).decode('utf-8').split('\n')
+                request_data = ''
+                json_data = {}
+                for element in request_data_arr:
+                    if len(element) > 0 and element[0] == '{':
+                        request_data = element
+                        break
+                if request_data != '':
+                    json_data = json.loads(request_data)
 
-        """ + bytes(str(res), 'utf-8')
-            client_connection.sendall(http_response)
-            client_connection.close()
+                # handle response
+                endpoint = json_data['endpoint']
+                endpoint_object = endpoint_objects[endpoint]
+                res = ''
+                try:
+                    res = endpoint_object.handle(json_data)
+                except Exception as e:
+                    res = str(e)
+                
+                # format and send response
+                http_response = b"""\
+            HTTP/1.1 200 OK
+            Access-Control-Allow-Origin: *
+
+            """ + bytes(str(res), 'utf-8')
+                client_connection.sendall(http_response)
+                client_connection.close()
+
+            except Exception as e:
+                print(str(e))
+                pass
